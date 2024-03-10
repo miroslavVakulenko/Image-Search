89604962162095543cf61659f1ae94336711450e
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-03-10 21:03:42 +0200

    start

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,30 @@
+// https://pixabay.com/api/?key=42512017-1bddfdd3afd2851258a10c68c&q=sex&image_type=photo&orientation=horizontal&safesearch=true
+
+import axios from 'axios';
+import { createMarkup } from '../main';
+const API_URL = 'https://pixabay.com/api/';
+const API_KEY = '42512017-1bddfdd3afd2851258a10c68c';
+
+const fetchImg = async (userRequest, page) => {
+  const response = await axios.get(
+    `${API_URL}?key=${API_KEY}&q=${userRequest}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${page}`,
+    {
+      params: {
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: page,
+        per_page: 15,
+      },
+    }
+  );
+  return response.data;
+};
+
+// function fetchImg(userRequest, per_page = 9) {
+//   return fetch(
+//     `${API_URL}?key=${API_KEY}&q=${userRequest}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}`
+//   ).then(res => res.json());
+// }
+
+export { fetchImg };
---
+++ src/main.js
@@ -0,0 +1,107 @@
+'use strict';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { fetchImg } from './js/pixabay-api';
+// import { fetchImg } from './js/render-functions';
+
+const gallerySimple = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+const btn = document.querySelector('button[type="submit"]');
+const imgList = document.querySelector('.images-list');
+const form = document.querySelector('.js-form');
+const jsInput = document.querySelector('input[name="js-input"]');
+const loader = document.querySelector('.loader');
+loader.style.display = 'none';
+
+form.addEventListener('submit', searchForm);
+
+function searchForm(evt) {
+  evt.preventDefault();
+  loader.style.display = 'block';
+  const inputValue = evt.currentTarget.elements['js-input'].value;
+  if (!inputValue) {
+    btn.disabled = true;
+    iziToast.show({
+      message: 'Please, enter the value!',
+    });
+  } else {
+    fetchImg(inputValue, 1)
+      .then(res => {
+        //if img not find show alert
+        if (res.total === 0) {
+          iziToast.show({
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            backgroundColor: 'rgb(239, 64, 64)',
+          });
+        } else {
+          //clear
+          imgList.innerHTML = '';
+          imgList.insertAdjacentHTML('beforeend', createMarkup(res.hits));
+          gallerySimple.refresh();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        iziToast.error({
+          class: 'popup-message',
+          theme: 'dark',
+          backgroundColor: '#ef4040',
+          messageColor: '#fff',
+          iconUrl: Error,
+          position: 'topRight',
+          pauseOnHover: true,
+          timeout: 3000,
+          message: `${err}`,
+        });
+      })
+      .finally(() => {
+        // Hide loader when image loading is finished
+        loader.style.display = 'none';
+      });
+  }
+}
+
+function createMarkup(images) {
+  return images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<li class="gallery__item">
+        <div class="gallery__card">
+          <a href="${largeImageURL}" class="gallery-card__link"
+            ><img src="${webformatURL}" alt="${tags}" class="gallery-card__image"
+          /></a>
+          <div class="gallery-card__description">
+            <p class="gallery-card__text">Likes <span>${likes}</span></p>
+            <p class="gallery-card__text">Views <span>${views}</span></p>
+            <p class="gallery-card__text">Comments <span>${comments}</span></p>
+            <p class="gallery-card__text">Downloads <span>${downloads}</span></p>
+          </div>
+        </div>
+      </li>`;
+      }
+    )
+    .join('');
+}
+//at repeated input unlock button
+jsInput.addEventListener('input', function () {
+  btn.disabled = false;
+});
+
+imgList.addEventListener('click', function (evt) {
+  evt.preventDefault();
+});
